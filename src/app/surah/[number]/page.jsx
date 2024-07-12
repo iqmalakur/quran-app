7eee28c3b2716b9b8d00709d0c86dfc38a568c94
iqmalakur <iakbarkurnia@gmail.com>
2024-07-12 15:06:25 +0700

    add redirect to '/' when access '/surah' without surah number

---
+++ src/app/surah/[number]/page.jsx
@@ -0,0 +1,13 @@
+export default function Page({params}){
+    // const router = useRouter();
+    // const dataSurah = await fetch('https://quran-api-id.vercel.app/surahs/112');
+    // const surah = await dataSurah.json();
+
+    console.log(params)
+
+    return (
+        <>
+            <h1>slug: </h1>
+        </>
+    );
+}
