55f60c8669866ed2ce9930d5a28388ccc18a4c07
ZaAtsmaro28 <fazaatsmaro@gmail.com>
2024-07-13 06:38:55 +0700

    modified data serve method && add keyword param

--- src/util/getAllSurah.js
+++ src/util/getAllSurah.js
@@ -1,8 +1,10 @@
-const getAllSurah = async () =>{
+const getAllSurah = async (keyword) =>{
     const data = await fetch("https://quran-api-id.vercel.app/surahs")
     const surahs = await data.json();
 
-    return surahs;
+    const filteredSurahs = surahs.filter((surah) => surah.name.toLowerCase().includes(keyword));
+
+    return filteredSurahs;
 }
 
 export default getAllSurah
