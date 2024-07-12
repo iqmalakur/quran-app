945a3005ac4b60e03bde081a67e719a54d44b939
ZaAtsmaro28 <fazaatsmaro@gmail.com>
2024-07-13 06:33:42 +0700

    add comps:searchbox,footer && move out search field from navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -22,22 +22,6 @@ export default function Navbar() {
                         </svg>
                         <span className="sr-only">Search</span>
                     </button>
-                    {pathname !== "/" ? "" :
-                        <div className="relative hidden md:block">
-                            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                                          strokeWidth="2"
-                                          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                </svg>
-                                <span className="sr-only">Search icon</span>
-                            </div>
-                            <input type="text" id="search-navbar"
-                                   className="block w-full p-2 ps-10 text-sm border rounded-lg bg-gray-50 focus:border-blue-500 dark:bg-gray-700 border-gray-600 placeholder-gray-400 text-black focus:ring-blue-500 dark:focus:border-blue-500"
-                                   placeholder="Search..."/>
-                        </div>
-                    }
                     <button data-collapse-toggle="navbar-search" type="button"
                             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                             aria-controls="navbar-search" aria-expanded="false">
