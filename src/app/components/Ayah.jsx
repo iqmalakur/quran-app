0e6ff2092f3983c4dadee3abb27f264a51ee1ee6
iqmalakur <iakbarkurnia@gmail.com>
2024-07-13 17:40:34 +0700

    create play surah feature

--- src/app/components/Ayah.jsx
+++ src/app/components/Ayah.jsx
@@ -1,6 +1,29 @@
-import {toArabic} from "arabic-digits";
+'use client'
 
-export default function Ayah({ number, arab, translation }) {
+import {toArabic} from "arabic-digits";
+import {useRef} from "react";
+
+export default function Ayah({ number, arab, translation, audio, currentSurah, onSurahChange }) {
+    const audioRef = useRef(null);
+    const playStatus = number === currentSurah;
+
+    if(audioRef.current) {
+        if(playStatus) {
+            audioRef.current.play();
+        } else {
+            audioRef.current.pause();
+            audioRef.current.currentTime = 0;
+        }
+    }
+
+    const handleOnClick = () => {
+        onSurahChange(playStatus ? null : number);
+    }
+
+    const handleOnEnded = () => {
+        onSurahChange(number + 1);
+    }
+
     return (
         <div className="py-12 px-5 shadow-md shadow-slate-900">
             <span className="block text-3xl text-right leading-loose">
@@ -8,7 +31,18 @@ export default function Ayah({ number, arab, translation }) {
                 <span className="inline-block mx-2"></span>
                 {number === 0 ? '' : `(${toArabic(number)})`}
             </span>
-            <span className="block mt-5 text-md text-left text-slate-400">{number === 0 ? '' : `${number}.`} {translation}</span>
+            <span
+                className="block mt-5 text-md text-left text-slate-400">{number === 0 ? '' : `${number}.`} {translation}</span>
+            <button
+                className={"flex justify-center items-center w-10 h-10 mt-3 text-slate-500 rounded-full " +
+                "hover:bg-[rgba(226,232,240,0.2)]"}
+                onClick={handleOnClick}>
+                <img src={playStatus ? "/pause.svg" : "/play.svg"} alt="play button" className="h-6" />
+            </button>
+            <audio ref={audioRef} onEnded={handleOnEnded}>
+                <source src={audio} type="audio/mpeg"/>
+                Your browser does not support the audio tag.
+            </audio>
         </div>
     )
 }
--- src/app/surah/[number]/page.jsx
+++ src/app/surah/[number]/page.jsx
@@ -1,5 +1,5 @@
 import getSurah from "@/util/getSurah";
-import Ayah from "@/app/components/Ayah";
+import AyahList from "@/app/components/AyahList";
 
 export default async function Surah({ params }){
     const number = params.number;
@@ -12,24 +12,7 @@ export default async function Surah({ params }){
                 <hr className="w-1/2 mt-3 mb-4 mx-auto border-slate-800"/>
                 <span className="block mx-32 mb-3 text-center text-sm text-slate-400">{surah.description}</span>
             </div>
-            <div className="mb-28 px-28">
-                {/* Don't start with bismillah for Al-Fatihah and At-Tawbah */}
-                {number === '1' || number === '9' ? '' :
-                    <Ayah
-                        number={0}
-                        arab={surah.bismillah.arab}
-                        translation={surah.bismillah.translation}
-                    />
-                }
-                {surah.ayahs.map(ayah =>
-                    <Ayah
-                        key={ayah.number.inQuran}
-                        number={ayah.number.inSurah}
-                        arab={ayah.arab}
-                        translation={ayah.translation}
-                    />
-                )}
-            </div>
+            <AyahList surah={surah} />
         </div>
     );
 }
