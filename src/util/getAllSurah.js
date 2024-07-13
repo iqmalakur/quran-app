92389296837e47836843efaf60416f9e5db91c43
ZaAtsmaro28 <fazaatsmaro@gmail.com>
2024-07-13 08:36:55 +0700

    search feature optimization

--- src/util/getAllSurah.js
+++ src/util/getAllSurah.js
@@ -2,7 +2,9 @@ const getAllSurah = async (keyword) =>{
     const data = await fetch("https://quran-api-id.vercel.app/surahs")
     const surahs = await data.json();
 
-    const filteredSurahs = surahs.filter((surah) => surah.name.toLowerCase().includes(keyword));
+    const cleanedKeyword = keyword.replace(/[^\w]/gi, '').toLowerCase();
+
+    const filteredSurahs = surahs.filter((surah) => surah.name.replace(/[^\w\s]/gi, '').toLowerCase().includes(cleanedKeyword));
 
     return filteredSurahs;
 }
